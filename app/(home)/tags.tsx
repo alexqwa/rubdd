fdd9e1d02e6ac24c196632c3d3661d5783efcb53
Alexandre Ferreira <breadmodz@gmail.com>
2024-11-28 17:43:31 -0300

    Added UX

--- app/(home)/tags.tsx
+++ app/(home)/tags.tsx
@@ -1,5 +1,5 @@
 import dayjs from "dayjs"
-import { View, Text } from "react-native"
+import { View, Text, ScrollView } from "react-native"
 import { Feather } from "@expo/vector-icons"
 
 import { Header } from "@/src/components/Header"
@@ -18,42 +18,47 @@ export default function Tags({ date }: TagsProps) {
   return (
     <View className="items-center bg-background flex-1">
       <Header title="Auditoria de Etiquetas" back={false} />
-      <View className="w-full flex-1 max-w-[90%] mt-10">
-        <View className="items-center w-full justify-between flex-row mb-5">
-          <Text className="text-white font-rajdhani_700 text-2xl">
-            Departamentos
-          </Text>
-          <View className="flex-col items-end">
-            <Text className="text-white font-rajdhani_700 text-sm">
-              {dateOfMonth}
+      <ScrollView
+        contentContainerStyle={{ alignItems: "center" }}
+        className="flex-1 w-full"
+      >
+        <View className="w-full flex-1 max-w-[90%] mt-10">
+          <View className="items-center w-full justify-between flex-row mb-5">
+            <Text className="text-white font-rajdhani_700 text-2xl">
+              Departamentos
             </Text>
-            <Text className="text-white font-rajdhani_700 text-sm">
-              {dayOfWeek}
-            </Text>
+            <View className="flex-col items-end">
+              <Text className="text-white font-rajdhani_700 text-sm">
+                {dateOfMonth}
+              </Text>
+              <Text className="text-white font-rajdhani_700 text-sm">
+                {dayOfWeek}
+              </Text>
+            </View>
+          </View>
+
+          {Array.from({ length: data.length }).map((_, i) => {
+            return (
+              <ListDepartaments
+                key={i}
+                title={data[i]}
+                address={data[i]}
+                active={false}
+              />
+            )
+          })}
+
+          <View className="border-dashed border rounded-lg border-zinc-400 w-full p-6 mt-10">
+            <View className="flex-row space-x-4 items-center justify-center">
+              <Feather name="alert-circle" size={24} color="#fff" />
+              <Text className="text-white/80 font-rajdhani_700 text-sm leading-snug">
+                Use as funções deste app com segurança. Tenha cuidado para
+                evitar problemas maiores!
+              </Text>
+            </View>
           </View>
         </View>
-
-        {Array.from({ length: data.length }).map((_, i) => {
-          return (
-            <ListDepartaments
-              key={i}
-              title={data[i]}
-              address={data[i]}
-              active={false}
-            />
-          )
-        })}
-
-        <View className="border-dashed border rounded-lg border-zinc-400 w-full p-6 mt-10">
-          <View className="flex-row space-x-4 items-center justify-center">
-            <Feather name="alert-circle" size={24} color="#fff" />
-            <Text className="text-white/80 font-rajdhani_700 text-sm leading-snug">
-              Use as funções deste app com segurança. Tenha cuidado para evitar
-              problemas maiores!
-            </Text>
-          </View>
-        </View>
-      </View>
+      </ScrollView>
     </View>
   )
 }
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,13 @@
 import { router } from "expo-router"
-import { Feather } from "@expo/vector-icons"
-import { View, Text, TextInput, TouchableOpacity } from "react-native"
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native"
 
 import { Header } from "@/src/components/Header"
 
@@ -8,45 +15,58 @@ export default function AuthHome() {
   return (
     <View className="bg-background flex-1 items-center">
       <Header title="Autenticação" back={false} />
-      <View className="flex-1 justify-center items-center w-full max-w-[90%]">
-        <Text className="text-white font-rajdhani_700 text-4xl mb-6">
-          RUB <Text className="text-green-500">UNLOCKED</Text>
-        </Text>
-        <Text className="text-white text-center font-rajdhani_700 text-xl">
-          Faça as mudanças necessárias para{"\n"}que o seu departamento fique
-          {"\n"}
-          sempre no verde!
-        </Text>
-
-        <View className="space-y-3 w-full mt-6">
-          <View className="relative">
-            <TextInput
-              editable={false}
-              selectionColor="#F7DD43"
-              placeholderTextColor="#8D8D99"
-              placeholder="HTTP://10.46.3.223:4280"
-              className="w-full bg-foreground text-[#8D8D99] placeholder:font-rajdhani_700 rounded-lg px-4 h-13 border border-outline"
-            />
-            <View className="h-2 w-2 rounded-full bg-green-500 absolute right-6 top-[21px]" />
-          </View>
-          <TextInput
-            selectionColor="#F7DD43"
-            placeholderTextColor="#8D8D99"
-            placeholder="DIGITE SEU CPF"
-            keyboardType="number-pad"
-            className="w-full bg-foreground text-[#8D8D99] placeholder:font-rajdhani_700 rounded-lg px-4 h-13 border border-outline"
-          />
-          <TouchableOpacity
-            activeOpacity={0.7}
-            onPress={() => router.replace("/(home)")}
-            className="bg-[#F7DD43] items-center justify-center w-full h-13 rounded-lg"
-          >
-            <Text className="uppercase text-black text-base font-rajdhani_700">
-              Conectar-se ao servidor
+      <KeyboardAvoidingView
+        className="flex-1 w-full"
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+      >
+        <ScrollView
+          contentContainerStyle={{
+            alignItems: "center",
+          }}
+          className="flex-1 w-full"
+        >
+          <View className="flex-1 items-center mt-[calc(300px/2)] w-full max-w-[90%]">
+            <Text className="text-white font-rajdhani_700 text-4xl mb-6">
+              RUB <Text className="text-green-500">UNLOCKED</Text>
             </Text>
-          </TouchableOpacity>
-        </View>
-      </View>
+            <Text className="text-white text-center font-rajdhani_700 text-xl">
+              Faça as mudanças necessárias para{"\n"}que o seu departamento
+              fique
+              {"\n"}
+              sempre no verde!
+            </Text>
+
+            <View className="space-y-3 w-full mt-6">
+              <View className="relative">
+                <TextInput
+                  editable={false}
+                  selectionColor="#F7DD43"
+                  placeholderTextColor="#8D8D99"
+                  placeholder="HTTP://10.46.3.223:4280"
+                  className="w-full bg-foreground text-[#8D8D99] placeholder:font-rajdhani_700 rounded-lg px-4 h-13 border border-outline"
+                />
+                <View className="h-2 w-2 rounded-full bg-green-500 absolute right-6 top-[21px]" />
+              </View>
+              <TextInput
+                selectionColor="#F7DD43"
+                placeholderTextColor="#8D8D99"
+                placeholder="DIGITE SEU CPF"
+                keyboardType="number-pad"
+                className="w-full bg-foreground text-[#8D8D99] placeholder:font-rajdhani_700 rounded-lg px-4 h-13 border border-outline"
+              />
+              <TouchableOpacity
+                activeOpacity={0.7}
+                onPress={() => router.replace("/(home)")}
+                className="bg-[#F7DD43] items-center justify-center w-full h-13 rounded-lg"
+              >
+                <Text className="uppercase text-black text-base font-rajdhani_700">
+                  Conectar-se ao servidor
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </ScrollView>
+      </KeyboardAvoidingView>
     </View>
   )
 }
